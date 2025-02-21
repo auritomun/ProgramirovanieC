151bca800d3364e7bd69cd117f3a7aa736b49d9c
Aurel_Myntany <aurel.myn@mail.ru>
2025-02-21 23:58:54 +1000

    lab1.c

--- lab1.c
+++ lab1.c
@@ -14,7 +14,6 @@ int main(int argc, const char * argv[]) {
         x2 = b*sqrt(d)/(2*a);
         printf("x1 %lf \n",x1);
         printf("x2 %lf \n",x2);
-        return 0;
     } else if(d == 0){
         x1 = -b*sqrt(d)/(2*a);
         printf("x1 %lf \n",x1);
